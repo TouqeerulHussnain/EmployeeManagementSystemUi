5bef1ac1850d1c5c412dc69e96b2457e7ad183b2
Touqeer <touqeerulhussnain@gmail.com>
2024-07-09 13:16:58 +0500

    setup the folder structure

---
+++ src/app/view/atendance-report/atendance-report.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atendance-report',
+  standalone: true,
+  imports: [],
+  templateUrl: './atendance-report.component.html',
+  styleUrl: './atendance-report.component.css'
+})
+export class AtendanceReportComponent {
+
+}
---
+++ src/app/view/check-in-check-out/check-in-check-out.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-check-in-check-out',
+  standalone: true,
+  imports: [],
+  templateUrl: './check-in-check-out.component.html',
+  styleUrl: './check-in-check-out.component.css'
+})
+export class CheckInCheckOutComponent {
+
+}
